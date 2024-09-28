5f35f0db742a25a75a85527dd3406422c3ab05b6
allen.li <595004044@qq.com>
2024-09-28 23:59:51 +0800

    Initial Commit

---
+++ HDPlayer/HDPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDPlayerApp.swift
+//  HDPlayer
+//
+//  Created by Allen on 2024/9/28.
+//
+
+import SwiftUI
+
+@main
+struct HDPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
